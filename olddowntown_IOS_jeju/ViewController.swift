f117d7933e1d9a0d62ebc9556ecec66c41c0a7c0
안치홍 <hong@anchihong-ui-MacBook-Pro.local>
2017-06-08 02:03:02 +0900

    Initial Commit

---
+++ olddowntown_IOS_jeju/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  olddowntown_IOS_jeju
+//
+//  Created by 안치홍 on 2017. 6. 8..
+//  Copyright © 2017년 creativeEconomy_jeju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
